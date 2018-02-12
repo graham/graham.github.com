cbf8619bc2294b936a7f36aad86e10123bcc66d1
Graham Abbott <graham.abbott@gmail.com>
2018-02-12 06:06:28 -0800

    boop

--- app/static/compiled/main.bundle.js
+++ app/static/compiled/main.bundle.js
@@ -1,5 +1,1 @@
-webpackJsonp([1],[/*!**********************!*\
-  !*** ./src/main.tsx ***!
-  \**********************/
-function(t,e,n){"use strict";var o=this&&this.__extends||function(t,e){function n(){this.constructor=t}for(var o in e)e.hasOwnProperty(o)&&(t[o]=e[o]);t.prototype=null===e?Object.create(e):(n.prototype=e.prototype,new n)},r=n(/*! react */1),c=n(/*! react-dom */1),p=function(t){function e(){t.apply(this,arguments)}return o(e,t),e.prototype.render=function(){return r.createElement("div",null," Hello World ")},e}(r.Component);c.render(r.createElement(p,null),document.getElementById("content"))}]);
-//# sourceMappingURL=main.bundle.js.map
+webpackJsonp([3],[function(t,e,n){"use strict";var o=this&&this.__extends||function(t,e){function n(){this.constructor=t}for(var o in e)e.hasOwnProperty(o)&&(t[o]=e[o]);t.prototype=null===e?Object.create(e):(n.prototype=e.prototype,new n)},r=n(1),c=n(1),p=function(t){function e(){t.apply(this,arguments)}return o(e,t),e.prototype.render=function(){return r.createElement("div",null," Hello World ")},e}(r.Component);c.render(r.createElement(p,null),document.getElementById("content"))}]);
